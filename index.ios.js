0ed1167dcb0d741e1e3320a5183493da6eccf7aa
黄保贤 <huangbaoxian@edaijia-inc.cn>
2018-02-07 22:01:46 +0800

    学习记录

--- index.ios.js
+++ index.ios.js
@@ -3,51 +3,90 @@
  * https://github.com/facebook/react-native
  * @flow
  */
+/*面向组件开发*/
+/*、ios的对象在rn中是组件*/
+/*AppRegistry 注册*/
+/*StyleSheet 样式*/
+/*Text 文本组件*/
+/*View 视图组件 */
+
+/*hBXRNLearning 最终会变成组件*/
+
+/*render() 初始化函数 viewdidload  return具体的内容*/
+
+/*const styles = StyleSheet.create 样式*/
+
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+    Image,
+    TextInput,
+
 } from 'react-native';
 
 export default class hBXRNLearning extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={myStyle.container}>
+
+
+          <Text>
+
+          wwwww
+
+          </Text>
+
+        <TextInput   style={myStyle.container}>
+
+        </TextInput>
+        {/*<Text style={styles.welcome}>*/}
+          {/*Welcome hell baby to React Native!*/}
+        {/*</Text>*/}
+        {/*<Text style={styles.instructions}>*/}
+          {/*To get started, edit index.ios.js*/}
+        {/*</Text>*/}
+        {/*<Text style={styles.instructions}>*/}
+          {/*Press Cmd+R to reload,{'\n'}*/}
+          {/*Cmd+D or shake for dev menu*/}
+        {/*</Text>*/}
       </View>
     );
   }
 }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+const myStyle = StyleSheet.create({
+    container: {
+        flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    backgroundColor: 'green',
+    }
+
+
 });
+
 
 AppRegistry.registerComponent('hBXRNLearning', () => hBXRNLearning);
